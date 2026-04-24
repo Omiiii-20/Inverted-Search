2955e3b80544f7eeea0370fcca1f3f5ca5337c30
Om Bidikar <ombidikar72@gmail.com>
2026-04-24 22:00:55 +0530

    Add files via upload

---
+++ include/inverted.h
@@ -0,0 +1,49 @@
+#ifndef INVERTED_H
+#define INVERTED_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define SIZE 27 // 26 letters + 1 for non-alphabet
+#define WORD_SIZE 100
+#define TABLE_SIZE 50
+
+
+typedef struct SubNode {
+    char filename[100];
+    int word_count;
+    struct SubNode *link;
+} SubNode;
+
+typedef struct MainNode {
+    char word[50];
+    int file_count;
+    SubNode *sub_link;
+    struct MainNode *link;
+} MainNode;
+
+typedef struct SList {
+    char filename[100];
+    struct SList *link;
+} SList;
+
+// From createdatabase.c
+void create_database(SList *file_list, MainNode *hashtable[]);
+int get_index(char *word);
+void to_lowercase(const char *src, char *dest);
+
+// From displaydatabase.c
+void display_database(MainNode *hashtable[]);
+
+// From searchdatabase.c
+void search_database(MainNode *hashtable[]);
+
+// From savedatabase.c
+void save_database(MainNode *hashtable[]);
+
+// From updatedatabase.c
+void update_database(MainNode *hashtable[]);
+
+#endif
